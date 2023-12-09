299b84cdcbee89f0290f8de26e411cc87ac4263b
Baran Akhavan - Yekta <200529109@student.georgianc.on.ca>
2023-12-08 20:12:13 -0500

    added additional bootstrap

--- scripts/index.js
+++ scripts/index.js
@@ -7,10 +7,10 @@ const tableBody = document.querySelector("#tableBody");
 const baseURL = 'https://lamp.computerstudi.es/~PriyanshPriyansh001/java/week14/posts.json';
 const apiKey =  "200529109";
 
-// Proxy URL
-const proxyURL = 'http://localhost:3000/proxy';
+// proxy URL to access info from the lamp.compuerstudie.es website (i downloaded node and all of the required tools in order to access the info)
+const proxyURL = 'http://localhost:3000/proxy'; // my localhost
 
-// generate complete url n do a fetch request
+// generate complete url and do a fetch request
 function fetchDataFromJson() {
     // generate the complete url
     let searchData = search.value;
@@ -41,16 +41,27 @@ function displayData(json) {
             let titleData = document.createElement("td");
             let idData = document.createElement("td");
             let bodyData = document.createElement("td");
+            let tagsData = document.createElement("td");
 
+            tableRow.setAttribute("class", "table-success");
             // add data in all the td tags
             titleData.textContent = storyData[i].title;
             idData.textContent = storyData[i].id;
             bodyData.textContent = storyData[i].body;
 
+            // Handle tags array
+            let tagsArray = storyData[i].tags;
+            if (tagsArray && tagsArray.length > 0) {
+                tagsData.textContent = tagsArray.join(', '); // Display tags as a comma-separated string
+            } else {
+                tagsData.textContent = 'No tags';
+            }
+
             // appending all the data to tr tags n adding tr to tbody tag
             tableRow.appendChild(idData);
             tableRow.appendChild(titleData);
             tableRow.appendChild(bodyData);
+            tableRow.appendChild(tagsData);
 
             tableBody.appendChild(tableRow);
         }
