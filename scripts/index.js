90feb2f4c9a872c1152faf85526885c878cdd5a6
Baran Akhavan - Yekta <200529109@student.georgianc.on.ca>
2023-12-08 20:03:22 -0500

    Information is being displayed successfully

--- scripts/index.js
+++ scripts/index.js
@@ -27,6 +27,36 @@ function fetchDataFromJson() {
 // a function to display data to page
 function displayData(json) {
     console.log(json);
+
+    const storyData = json.posts;
+
+    // Check if storyData is defined before looping through it
+    if (storyData && storyData.length > 0) {
+        // looping through each object from the array
+        for (let i = 0; i < storyData.length; i++) {
+            console.log(storyData[i]);
+
+            // tr and td elements for the body tag
+            let tableRow = document.createElement("tr");
+            let titleData = document.createElement("td");
+            let idData = document.createElement("td");
+            let bodyData = document.createElement("td");
+
+            // add data in all the td tags
+            titleData.textContent = storyData[i].title;
+            idData.textContent = storyData[i].id;
+            bodyData.textContent = storyData[i].body;
+
+            // appending all the data to tr tags n adding tr to tbody tag
+            tableRow.appendChild(idData);
+            tableRow.appendChild(titleData);
+            tableRow.appendChild(bodyData);
+
+            tableBody.appendChild(tableRow);
+        }
+    } else {
+    console.error('No Data Has Been Found!');
+    }
 }
 
 // add event listener on the submit button to call fetchDataFromJson
